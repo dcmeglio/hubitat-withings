cfd34ec536c0be04403761b07e52229b522b0680
lnjustin <lnjustin@users.noreply.github.com>
2022-05-29 22:39:24 -0400

    Update Withings_User.groovy

--- apps/Withings_User.groovy
+++ apps/Withings_User.groovy
@@ -4,8 +4,17 @@
  *
  *  Copyright 2020 Dominick Meglio
  *
- *	If you find this useful, donations are always appreciated 
- *	https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7LBRPJRLJSDDN&source=url
+ *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *  v1.6.0 - Released under Apache License
  */
 
  import groovy.transform.Field
